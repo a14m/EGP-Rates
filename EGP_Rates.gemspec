367378135da51c38948035c4275b1e1dd9878477
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-11-11 00:19:34 +0100

    CHORE: configure the spec helper vcr, webmock and tags used

--- EGP_Rates.gemspec
+++ EGP_Rates.gemspec
@@ -23,6 +23,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'pry-byebug'
   spec.add_development_dependency 'rubocop'
   spec.add_development_dependency 'rubocop-rspec'
+  spec.add_development_dependency 'vcr'
+  spec.add_development_dependency 'webmock'
 
   spec.add_dependency 'nokogiri'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,34 @@
 # frozen_string_literal: true
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'EGP_Rates'
+require 'pry'
+require 'webmock/rspec'
+require 'vcr'
+
+WebMock.disable_net_connect!
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.allow_http_connections_when_no_cassette = false
+  config.configure_rspec_metadata!
+end
+
+RSpec.configure do |config|
+  config.order = 'random'
+
+  config.around(:each) do |ex|
+    if ex.metadata.key?(:live)
+      WebMock.allow_net_connect!
+      VCR.turned_off { ex.run }
+      WebMock.disable_net_connect!
+    elsif ex.metadata.key?(:no_vcr)
+      VCR.turned_off { ex.run }
+    elsif ex.metadata.key?(:vcr)
+      VCR.configuration.default_cassette_options[:record] = :once
+      ex.run
+    else
+      ex.run
+    end
+  end
+end
