2fe52328fb505dcff34b839937dd611a339b9e76
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-11-12 19:59:49 +0100

    DOCS: fix rubocop violations

--- lib/egp_rates/bank.rb
+++ lib/egp_rates/bank.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
-# Class Representing the bank to get the data from
 module EGPRates
+  # Class Representing the bank to get the data from
   class Bank
     attr_reader :sym
 
     # Abstract method
     # Subclasses banks define the logic to get the exchange rates hash
-    # it should return [Hash] { { sell: { SYM: rate } }, { buy: { SYM: rate } } }
+    # it should return [Hash] sell: { SYM: rate, ... } , buy: { SYM: rate, ... }
     #   for the available currencies (represented by :SYM) on the bank pages
     def exchange_rates
       raise NotImplementedError
