fbf9626eadca28e4493f5717abf8cce3fe3dd144
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-11-12 21:42:17 +0100

    FEAT: add CIB scraping of exchange_rates

--- lib/egp_rates.rb
+++ lib/egp_rates.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 require 'oga'
+require 'json'
 require 'egp_rates/bank'
 require 'egp_rates/cbe'
 require 'egp_rates/nbe'
+require 'egp_rates/cib'
 
 # Base Module
 module EGPRates
