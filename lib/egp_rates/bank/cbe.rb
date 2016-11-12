664cd4a636b052faa827fbad14dd80d338b614e0
Raz <abdelrazzak.ahmed@gmail.com>
2016-11-12 17:46:44 +0100

    Feature/cbe (#1)
    Central Bank of Egypt scraper
    http://www.cbe.org.eg/en/EconomicResearch/Statistics/Pages/ExchangeRatesListing.aspx

---
+++ lib/egp_rates/bank/cbe.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+# Central Bank of Egypt
+class CBE < Bank
+  def initialize
+    @sym = :CBE
+    @uri = URI.parse('http://www.cbe.org.eg/en/EconomicResearch/Statistics/Pages/ExchangeRatesListing.aspx')
+  end
+
+  # @return [Hash] of exchange rates for selling and buying
+  #   {
+  #     { sell: { SYM: rate }, { SYM: rate }, ... },
+  #     { buy:  { SYM: rate }, { SYM: rate }, ... }
+  #   }
+  def exchange_rates
+    @exchange_rates ||= parse(raw_exchange_rates)
+  end
+
+  private
+
+  # Send the request to the URL and retrun raw data of the response
+  # @return [Enumerator::Lazy] with the table row in HTML that evaluates to
+  #   [
+  #     "US Dollar \r\n  ", "15.7297", "16.3929",
+  #     "Euro \r\n,      ", "17.5308", "18.1938",
+  #     ...
+  #   ]
+  #
+  def raw_exchange_rates
+    response = Net::HTTP.get_response(@uri)
+    fail ResponseError, response.code unless response.is_a? Net::HTTPSuccess
+    table_rows = Oga.parse_html(response.body).css('tbody').last&.children
+    fail ResponseError, 'Unknown HTML' unless table_rows&.size == 9
+    table_rows.lazy.map(&:children).map { |table_cell| table_cell.map(&:text) }
+  end
+
+  # Convert currency string to ISO symbol
+  # @param currency [String] "US Dollar"
+  # @return [Symbol] :USD ISO currency name
+  # rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/MethodLength
+  def currency_symbol(currency)
+    case currency
+    when /US/        then :USD
+    when /Euro/      then :EUR
+    when /Pound/     then :GBP
+    when /Swiss/     then :CHF
+    when /Japanese/  then :JPY
+    when /Saudi/     then :SAR
+    when /Kuwait/    then :KWD
+    when /UAE/       then :AED
+    when /Chinese/   then :CNY
+    else fail ResponseError, "Unknown currency #{currency}"
+    end
+  end
+  # rubocop:enable Metrics/CyclomaticComplexity
+  # rubocop:enable Metrics/MethodLength
+
+  # Parse the #raw_exchange_rates returned in response
+  # @return [Hash] of exchange rates for selling and buying
+  #   {
+  #     { sell: { SYM: rate }, { SYM: rate }, ... },
+  #     { buy:  { SYM: rate }, { SYM: rate }, ... }
+  #   }
+  def parse(raw_data)
+    raw_data.each_with_object(sell: {}, buy: {}) do |row, result|
+      sell_rate = row.pop.to_f
+      buy_rate  = row.pop.to_f
+      currency  = currency_symbol(row.pop)
+
+      result[:sell][currency] = sell_rate
+      result[:buy][currency]  = buy_rate
+    end
+  end
+end
