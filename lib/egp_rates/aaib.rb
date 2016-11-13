a63ed992172ee88f334b9a1d16b167da85c5799e
Raz <abdelrazzak.ahmed@gmail.com>
2016-11-13 11:14:19 +0100

    Feature/AAIB (#4)
    Scrape AAIB
    http://aaib.com/services/rates

---
+++ lib/egp_rates/aaib.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+module EGPRates
+  # Arab African International Bank
+  class AAIB < EGPRates::Bank
+    def initialize
+      @sym = :AAIB
+      @uri = URI.parse('http://aaib.com/services/rates')
+    end
+
+    # @return [Hash] of exchange rates for selling and buying
+    #   {
+    #     { sell: { SYM: rate }, { SYM: rate }, ... },
+    #     { buy:  { SYM: rate }, { SYM: rate }, ... }
+    #   }
+    def exchange_rates
+      @exchange_rates ||= parse(raw_exchange_rates)
+    end
+
+    private
+
+    # Send the request to the URL and retrun raw data of the response
+    # @return [Enumerator::Lazy] with the table row in HTML that evaluates to
+    #   [
+    #     [
+    #       "\r\n ",
+    #       "US DOLLAR"
+    #       "\r\n "
+    #       "15.9"
+    #       "\r\n "
+    #       "16.05"
+    #       "\r\n"
+    #     ], [
+    #       "\r\n "
+    #       "EURO CURRENCY"
+    #       "\r\n "
+    #       "17.2213"
+    #       "\r\n "
+    #       "17.5314"
+    #       "\r\n "
+    #     ], [
+    #       ...
+    #     ]
+    def raw_exchange_rates
+      response = Net::HTTP.get_response(@uri)
+      fail ResponseError, response.code unless response.is_a? Net::HTTPSuccess
+      response = response.body&.gsub("\u0000", '')
+      # AAIB provide 7 currencies only
+      table_rows = Oga.parse_html(response).css('#rates-table tr')
+      fail ResponseError, 'Unknown HTML' unless table_rows&.size == 7
+      table_rows.lazy.map(&:children).map { |cell| cell.map(&:text) }
+    end
+
+    # Convert currency string to ISO symbol
+    # @param currency [String] "US Dollar"
+    # @return [Symbol] :USD ISO currency name
+    # rubocop:disable Metrics/CyclomaticComplexity
+    def currency_symbol(currency)
+      case currency
+      when /US/        then :USD
+      when /EURO/      then :EUR
+      when /STERLING/  then :GBP
+      when /SWISS/     then :CHF
+      when /SAUDI/     then :SAR
+      when /KUWAIT/    then :KWD
+      when /DIRHAM/    then :AED
+      else fail ResponseError, "Unknown currency #{currency}"
+      end
+    end
+    # rubocop:enable Metrics/CyclomaticComplexity
+
+    # Parse the #raw_exchange_rates returned in response
+    # @return [Hash] of exchange rates for selling and buying
+    #   {
+    #     { sell: { SYM: rate }, { SYM: rate }, ... },
+    #     { buy:  { SYM: rate }, { SYM: rate }, ... }
+    #   }
+    def parse(raw_data)
+      raw_data.each_with_object(sell: {}, buy: {}) do |row, result|
+        sell_rate = row[5].to_f
+        buy_rate  = row[3].to_f
+        currency  = currency_symbol(row[1])
+
+        result[:sell][currency] = sell_rate
+        result[:buy][currency]  = buy_rate
+      end
+    end
+  end
+end
---
+++ spec/egp_rates/aaib_spec.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+describe EGPRates::AAIB do
+  subject(:bank) { described_class.new }
+
+  it 'Live Testing', :live do
+    expect(bank.exchange_rates).to include(:buy, :sell)
+    expect(bank.exchange_rates[:buy].size).to eq 7
+    expect(bank.exchange_rates[:sell].size).to eq 7
+  end
+
+  describe '.new' do
+    it 'initialize instance variables' do
+      expect(bank.sym).to eq :AAIB
+      expect(bank.instance_variable_get(:@uri)).to be_a URI
+    end
+  end
+
+  describe '#exchange_rates', vcr: { cassette_name: :AAIB } do
+    it 'calls #parse with #raw_exchange_rates' do
+      expect(bank).to receive(:raw_exchange_rates)
+      expect(bank).to receive(:parse)
+      bank.exchange_rates
+    end
+  end
+
+  describe '#raw_exchange_rates' do
+    it 'raises ResponseError unless Net::HTTPSuccess', :no_vcr do
+      stub_request(:get, /.*aaib.*/).to_return(body: '', status: 500)
+      expect { bank.send(:raw_exchange_rates) }.to raise_error\
+        EGPRates::ResponseError, '500'
+    end
+
+    it 'raises ResponseError if HTML structure changed', :no_vcr do
+      stub_request(:get, /.*aaib.*/).to_return(body: '', status: 200)
+      expect { bank.send(:raw_exchange_rates) }.to raise_error\
+        EGPRates::ResponseError, 'Unknown HTML'
+    end
+
+    it 'returns <#Enumerator::Lazy> of 7 rows', vcr: { cassette_name: :AAIB } do
+      lazy_enumerator = bank.send(:raw_exchange_rates)
+      expect(lazy_enumerator).to be_a Enumerator::Lazy
+      expect(lazy_enumerator.size).to eq 7
+    end
+  end
+
+  describe '#currency_symbol' do
+    %w(US EURO STERLING SWISS SAUDI KUWAIT DIRHAM)\
+      .each do |currency|
+      it "returns currency :SYM for #{currency}" do
+        symbols = %i(USD EUR GBP CHF SAR KWD AED)
+        expect(symbols).to include(bank.send(:currency_symbol, currency))
+      end
+    end
+
+    it 'raises ResponseError when Unknown Currency' do
+      expect { bank.send(:currency_symbol, 'EGYPTIAN POUND') }.to raise_error\
+        EGPRates::ResponseError, 'Unknown currency EGYPTIAN POUND'
+    end
+  end
+
+  describe '#parse', vcr: { cassette_name: :AAIB } do
+    let(:raw_data) { bank.send(:raw_exchange_rates) }
+
+    it 'returns sell: hash of selling prices' do
+      expect(bank.send(:parse, raw_data)[:sell]).to include(
+        AED: 4.3697,
+        CHF: 16.3259,
+        EUR: 17.5314,
+        GBP: 20.3402,
+        KWD: 53.2869,
+        SAR: 4.2797,
+        USD: 16.05
+      )
+    end
+
+    it 'returns buy: hash of buying prices' do
+      expect(bank.send(:parse, raw_data)[:buy]).to include(
+        AED: 4.3043,
+        CHF: 16.0687,
+        EUR: 17.2213,
+        GBP: 19.9609,
+        KWD: 52.2056,
+        SAR: 4.2157,
+        USD: 15.9
+      )
+    end
+  end
+end
