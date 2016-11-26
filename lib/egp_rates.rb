8a62427960e5fe9bca51b072eedd4e45def1d2f7
Ahmed Abdel-Razzak <abdelrazzak.ahmed@gmail.com>
2016-11-26 20:16:04 +0100

    FEAT: add API methods to interact with the underlying banks
    exchange_rates, loops over all the banks and fetches the data in
    threaded execution without blowing up when one bank fails
    
    exchange_rate(currency) is used to get the data from the last big
    response in an easier more readable way

--- lib/egp_rates.rb
+++ lib/egp_rates.rb
@@ -2,6 +2,7 @@
 
 require 'oga'
 require 'json'
+require 'thread'
 require 'egp_rates/bank'
 require 'egp_rates/cbe'
 require 'egp_rates/nbe'
@@ -28,5 +29,57 @@ module EGPRates
   # Class Idicating HTTPResponseErrors when unexpected behaviour encountered
   # while scraping the [Bank] data
   class ResponseError < StandardError
+  end
+
+  # Threaded execution to get the exchange rates from different banks
+  # @return [Hash] of the exchange rates of different banks
+  #   {
+  #     BANK: {
+  #       sell: { SYM: rate, SYM: rate },
+  #       buy:  { SYM: rate, SYM: rate }
+  #     },
+  #     BANK: {
+  #       sell: { SYM: rate, SYM: rate },
+  #       buy:  { SYM: rate, SYM: rate }
+  #     },
+  #   }
+  def self.exchange_rates
+    semaphore = Mutex.new
+    # Fetch all the constants (Banks) implemented
+    (constants - [:Bank, :ResponseError]).each_with_object({}) do |klass, rates|
+      Thread.new do
+        bank = EGPRates.const_get(klass).new
+
+        semaphore.synchronize do
+          begin
+            rates[klass] = bank.exchange_rates
+          rescue ResponseError
+            rates[klass] = 'Failed to get exchange rates'
+          end
+        end
+      end.join
+    end
+  end
+
+  # Return the exchange rate of a single currency from different banks
+  # @param [Symbol] sym, 3 charachter ISO symbol of the currency
+  # @param [Boolean] cache_result, a boolean to indicate whether or not
+  #   the result of the exchange_rates call be cached
+  # @return [Hash] of the exchange rates in different banks for a specific
+  #   currency
+  def self.exchange_rate(sym = :USD, cache_result = true)
+    @exchange_rates ||= exchange_rates if cache_result
+    @exchange_rates   = exchange_rates unless cache_result
+
+    @exchange_rates.each_with_object({}) do |rates, result|
+      begin
+        result[rates[0]] = {
+          sell: rates[1][:sell].fetch(sym.upcase, 'Not Available'),
+          buy:  rates[1][:buy].fetch(sym.upcase,  'Not Available')
+        }
+      rescue TypeError
+        result[rates[0]] = rates[1]
+      end
+    end
   end
 end
