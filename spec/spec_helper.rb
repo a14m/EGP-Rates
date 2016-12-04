381da33af2037a98b765b9d4ed9f3258f7154abf
Raz <abdelrazzak.ahmed@gmail.com>
2016-12-04 15:51:56 +0100

    FEAT: serialize cassettes as gz compressed files to save space

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,7 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.allow_http_connections_when_no_cassette = false
   config.configure_rspec_metadata!
+  config.default_cassette_options[:serialize_with] = :compressed
 end
 
 RSpec.configure do |config|
